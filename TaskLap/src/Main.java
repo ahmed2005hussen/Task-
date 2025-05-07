75755d37d856b40f0cd1e00df8c8d5db0ee2b4a6
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2025-05-07 19:06:49 +0300

    menna abdelgwad fatthy 20230423

--- TaskLap/src/Main.java
+++ TaskLap/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Ahmed hussein Ahmed Mohammed : 20230015");
+        System.out.println("menna abdelgwad fathy : 20230423");
+        
 
     }
 }
